ef4826f950c9f5d2d02e72f44255dc71c1203d1d
Kayla Copeland <kaylabebusy@gmail.com>
2023-03-11 21:34:09 -0500

    lab complete

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name) {
+    const string = `Hi, my name is ${name}.`;
+    return string;
+}
+
+function introductionWithLanguage(name, language) {
+    const phrase = `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return phrase;
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    const phrase = `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return phrase;
+}
